3310cc82363da5d65c78efd786d2de98cf828dcd
Burtsev Vlad <designeedgames@yandex.ru>
2022-09-25 15:48:51 +0700

    added discount counter and sort && filter for list

--- Jumper/Domain/Models/Agent.cs
+++ Jumper/Domain/Models/Agent.cs
@@ -24,13 +24,34 @@ namespace Jumper
         public string? Email { get; set; }
         public string? Logo { get; set; }
         public int Priority { get; set; }
-        public string Discount => String.Concat((new Random().Next(0, 35).ToString()), "%");
+        public string Discount => GetDiscount();
+        public decimal SumSale => ProductSales.Sum(sum => sum.ProductCount * sum.Product.MinCostForAgent);
         public int ProductCount => ProductSales.Select(sale => sale.ProductCount).Sum();
-
 
         public virtual AgentType AgentType { get; set; } = null!;
         public virtual ICollection<AgentPriorityHistory> AgentPriorityHistories { get; set; }
         public virtual ICollection<ProductSale> ProductSales { get; set; }
         public virtual ICollection<Shop> Shops { get; set; }
+
+        private string GetDiscount()
+        {
+            if (SumSale >= 0 && SumSale <= 10000)
+                return "0%";
+
+            if (SumSale >= 10000 && SumSale <= 50000)
+                return "5%";
+
+            if (SumSale >= 50000 && SumSale <= 150000)
+                return "10%";
+
+            if (SumSale >= 150000 && SumSale <= 500000)
+                return "20%";
+
+            if (SumSale >= 500000)
+                return "25%";
+
+            else
+                return "0%";
+        }
     }
 }
--- Jumper/Presentation/Pages/AgentPage.xaml.cs
+++ Jumper/Presentation/Pages/AgentPage.xaml.cs
@@ -25,6 +25,10 @@ namespace Jumper.Presentation.Pages
     public partial class AgentPage : Page
     {
         private List<Agent> _ListAgent;
+        private string _SearchParameter = String.Empty;
+        private int _SortParameter = 0;
+        private int _FilterParameter = 0;
+        private const string _PATH = @"D:\Пользователи\Рабочий стол\Попрыженок - агенты";
 
         public AgentPage()
         {
@@ -34,25 +38,12 @@ namespace Jumper.Presentation.Pages
 
         private void LoadAgent()
         {
-            const string PATH = @"D:\Пользователи\Рабочий стол\Попрыженок - агенты";
             _ListAgent = new JumperContext().Agents.ToList();
 
             foreach (var agent in _ListAgent)
-                agent.Logo = String.Concat(PATH, agent.Logo);
+                agent.Logo = String.Concat(_PATH, agent.Logo);
 
-            ListView.ItemsSource = _ListAgent;
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (SearchTxtBox.Text == String.Empty)
-            {
-                ListView.ItemsSource = _ListAgent;
-                return;
-            }
-
-            ListView.ItemsSource = _ListAgent.FindAll(agent
-                => $"{agent.Title} {agent.Email} {agent.Phone}".Contains(SearchTxtBox.Text));
+            AgentListView.ItemsSource = _ListAgent;
         }
 
         private void ButtonAddClick(object sender, RoutedEventArgs e)
@@ -63,41 +54,64 @@ namespace Jumper.Presentation.Pages
 
         private void ButtonEditClick(object sender, RoutedEventArgs e)
         {
-            if (ListView.SelectedIndex != -1)
+            if (AgentListView.SelectedIndex != -1)
             {
-                new AgentEditWindow(((Agent)ListView.SelectedItem).Id).ShowDialog();
+                new AgentEditWindow(((Agent)AgentListView.SelectedItem).Id).ShowDialog();
                 LoadAgent();
             }
         }
 
         private void ButtonDeleteClick(object sender, RoutedEventArgs e)
         {
-            if (ListView.SelectedIndex != -1)
+            if (AgentListView.SelectedIndex != -1)
             {
                 var dbContext = new JumperContext();
-                var SelectedAgent = dbContext.Agents.FirstOrDefault(Agent => Agent.Id == ((Agent)ListView.SelectedItem).Id);
+                var SelectedAgent = dbContext.Agents.FirstOrDefault(Agent => Agent.Id == ((Agent)AgentListView.SelectedItem).Id);
                 dbContext.Remove(SelectedAgent);
                 dbContext.SaveChanges();
                 LoadAgent();
             }
         }
 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _SearchParameter = SearchTxtBox.Text;
+            SetFilterAndSortType();
+        }
+
         private void ComboBoxFilters_DropDownClosed(object sender, EventArgs e)
         {
-            if (ComboBoxSortType.SelectedIndex == 0)
-                ListView.ItemsSource = _ListAgent.OrderBy(agent => agent.AgentType.Title);
-
-            else
-                ListView.ItemsSource = _ListAgent.Where(Agent => Agent.AgentType.Title == ComboBoxFilters.Text);
+            _FilterParameter = ComboBoxFilters.SelectedIndex;
+            SetFilterAndSortType();
         }
 
         private void ComboBoxSortType_DropDownClosed(object sender, EventArgs e)
         {
-            if (ComboBoxSortType.SelectedIndex == 0)
-                ListView.ItemsSource = _ListAgent.OrderBy(agent => agent.Title);
+            _SortParameter = ComboBoxSortType.SelectedIndex;
+            SetFilterAndSortType();
+        }
 
-            if (ComboBoxSortType.SelectedIndex == 1)
-                ListView.ItemsSource = _ListAgent.OrderByDescending(agent => agent.Title);
+        private void SetFilterAndSortType()
+        {
+            if (_SortParameter == 0 && _FilterParameter == 0)
+                AgentListView.ItemsSource = _ListAgent
+                    .Where(agent => $"{agent.Title} {agent.Email} {agent.Phone}".Contains(_SearchParameter))
+                    .OrderBy(agent => agent.Title);
+
+            if (_SortParameter == 1 && _FilterParameter == 0)
+                AgentListView.ItemsSource = _ListAgent
+                    .Where(agent => $"{agent.Title} {agent.Email} {agent.Phone}".Contains(_SearchParameter))
+                    .OrderByDescending(agent => agent.Title);
+
+            if (_SortParameter == 0 && _FilterParameter != 0)
+                AgentListView.ItemsSource = _ListAgent
+                    .Where(agent => $"{agent.Title} {agent.Email} {agent.Phone}".Contains(_SearchParameter) && agent.AgentType.Title == ComboBoxFilters.Text)
+                    .OrderBy(agent => agent.Title);
+
+            if (_SortParameter == 1 && _FilterParameter != 0)
+                AgentListView.ItemsSource = _ListAgent
+                    .Where(agent => $"{agent.Title} {agent.Email} {agent.Phone}".Contains(_SearchParameter) && agent.AgentType.Title == ComboBoxFilters.Text)
+                    .OrderByDescending(agent => agent.Title);
         }
     }
 }
